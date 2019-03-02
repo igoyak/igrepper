41190e198d7edaba464146811de309b796d70e4d
Gustav Larsson <gustav.e.larsson@gmail.com>
2019-03-02 20:43:31 +0200

    Context support
    Bug fixes
    Refactoring

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ or copy `./igrepper/igrepper.py` into your `PATH`.
 Recommended bash alias:
 
     alias i='igrepper'
-    iman() { man "$1" | igrepper -c 4}
+    iman() { man "$1" | igrepper -c 3}
 
 ## Usage
 
@@ -31,8 +31,11 @@ __Commands__:
     ctrl-g: Copy grep command to clipboard
     ctrl-n: New subsearch
     ctrl-p: Cancel subsearch
-    ctrl-i: Toggle case sensitivity
+    ctrl-v: Toggle case sensitivity
+    ctrl-t: Increase context lines
+    ctrl-r: Decrease context lines
     Up/Down/PageUp/PageDown: scroll
+    ctrl-u/ctrl-d: half-page scroll
     
 
     
--- igrepper/igrepper.py
+++ igrepper/igrepper.py
@@ -7,25 +7,32 @@ from subprocess import Popen, PIPE
 import os
 import sys
 import argparse
+from typing import List
 
 BOLD = '\033[1m'
 INVERTED = '\033[7m'
 RESET = '\033[0m'
 AVAILABLE_COLORS = 6
-
 FOOTER_LINE_NO = 2
-
 WORD_REGEX = '\\S+'
 
+CTRL_D = 4
 CTRL_G = 7
 CTRL_H = 8
 CTRL_I = 9
+CTRL_J = 10
 CTRL_K = 11
+CTRL_L = 12
 CTRL_N = 14
 CTRL_O = 15
 CTRL_P = 16
-CTRL_J = 10
+CTRL_R = 18
+CTRL_T = 20
+CTRL_U = 21
+CTRL_V = 22
 CTRL_Y = 25
+CTRL_9 = 57
+CTRL_8 = 263
 
 
 class DisplayMode(Enum):
@@ -48,25 +55,33 @@ class Match:
         self.unique_id = None
 
 
+class Line:
+    def __init__(self, orig_line_no: int, line_text: str, line_matches: List[Match], break_line=False) -> None:
+        self.orig_line_no = orig_line_no
+        self.line_text = line_text
+        self.line_matches = line_matches
+        self.break_line = break_line
+
+
 class Search:
     """
     Object created from input, keeps track of regex, matches, settings
     """
 
-    def __init__(self, input_lines):
+    def __init__(self, input_lines: List[str], initial_context: int = 0) -> None:
         self.valid = True
-        self.lines = []
+        self.lines: List[List[Match]] = []
         self._input_lines = input_lines
         self.match_count = 0
         self.number_of_matched_lines = 0
         self.unique_match_count = 0
         self.selected_match = 0
-        self.unique_matches = []
-        self.previous_searches = []
+        self.unique_matches: List[str] = []
+        self.previous_searches: List[Search] = []
         self.ignore_case = True
         self.regex = ''
-        self.matched_lines = []
-        self.line_highlights = []
+        self.output_lines: List[Line] = []
+        self.context: int = initial_context
 
     def is_empty(self):
         return len(self.regex) == 0
@@ -79,22 +94,12 @@ class Search:
         if self.unique_matches:
             self.selected_match = (self.selected_match - 1 + self.unique_match_count) % self.unique_match_count
 
-    def output_lines(self):
-        if self.is_empty():
-            return self._input_lines
-        if not self.valid:
-            raise Exception()
-        matched_lines = []
-        for lineno, line in enumerate(self.lines):
-            if line:
-                matched_lines.append(self._input_lines[lineno])
-        return matched_lines
-
-    def update(self, regex):
+    def update(self, regex: str):
         self.regex = regex
         if self.is_empty():
-            self.matched_lines = self._input_lines
-            self.line_highlights = []
+            self.output_lines = []
+            for orig_line_no, line in enumerate(self._input_lines):
+                self.output_lines.append(Line(orig_line_no, line, []))
             return
         try:
             if self.ignore_case:
@@ -105,17 +110,34 @@ class Search:
         except re.error:
             self.valid = False
             return
-        # lines: list of lines. Each line is a list of matches
-        self.lines = [[Match(x) for x in rg.finditer(line)] for line in self._input_lines]
-        matched_lines_dict = {}  # map input line numbers to matched lines
-        line_highlights_dict = {}
-        for line_number, line in enumerate(self.lines):
-            if line:
-                matched_lines_dict[line_number] = self._input_lines[line_number]
-                line_highlights_dict[line_number] = line
+        self.output_lines = []
+        output_lines_dict = {}
+        self.lines = [[Match(x) for x in rg.finditer(l)] for l in self._input_lines]
+        for orig_line_number, matches_on_line in enumerate(self.lines):
+            if matches_on_line:
+                match_line = Line(orig_line_number, self._input_lines[orig_line_number], matches_on_line)
+                output_lines_dict[orig_line_number] = match_line  # Override possible context line with a match line
 
-        self.matched_lines = [matched_lines_dict[x] for x in sorted(matched_lines_dict.keys())]
-        self.line_highlights = [line_highlights_dict[x] for x in sorted(line_highlights_dict.keys())]
+                # Add context lines, including break lines
+                if self.context > 0:
+                    first_context_line = True
+                    for context_line_no in range(orig_line_number - self.context, orig_line_number + self.context + 1):
+                        if context_line_no < 0 or context_line_no >= len(self._input_lines):
+                            continue
+                        if context_line_no in output_lines_dict:
+                            # Already exists
+                            continue
+                        if first_context_line and len(output_lines_dict) > 1 and context_line_no > 1 \
+                                and context_line_no - 2 not in output_lines_dict:
+                            # create break
+                            b = Line(context_line_no - 1, '', [], break_line=True)
+                            output_lines_dict[context_line_no - 1] = b
+                        first_context_line = False
+                        output_lines_dict[context_line_no] = Line(context_line_no, self._input_lines[context_line_no],
+                                                                  [])
+
+        self.output_lines = [output_lines_dict[x] for x in sorted(output_lines_dict.keys())]
+
         matches = [match for line in self.lines for match in line]
         nonempty_matches = [match for match in matches if match]
         self.unique_matches = []
@@ -132,9 +154,9 @@ class Search:
 
 
 class IGrepper:
-    def __init__(self, input_data, regex=''):
+    def __init__(self, input_lines: List[str], regex='', initial_context=0) -> None:
         self.win = curses.initscr()
-        self.win.keypad(1)
+        self.win.keypad(True)
         curses.start_color()
 
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
@@ -145,14 +167,15 @@ class IGrepper:
         curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_BLACK)
         curses.init_pair(30, curses.COLOR_RED, curses.COLOR_WHITE)
         curses.init_pair(31, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.init_pair(32, curses.COLOR_BLACK, curses.COLOR_BLACK)
         curses.use_default_colors()
 
         self.regex = regex
-        self.input_lines = input_data
+        self.input_lines = input_lines
         self.display_mode = DisplayMode.show_default
         self.pager_ypos = 0
         self.pager_xpos = 0
-        self.search = Search(self.input_lines)
+        self.search = Search(self.input_lines, initial_context=initial_context)
         self.quit = False
 
     def toggle_display_mode(self):
@@ -162,6 +185,12 @@ class IGrepper:
             self.display_mode = DisplayMode.show_all
         elif self.display_mode == DisplayMode.show_all:
             self.display_mode = DisplayMode.show_default
+
+    def get_number_of_pager_lines(self):
+        maxy, _ = self.win.getmaxyx()
+        header_line_no = len(self.search.previous_searches) + 2
+        pager_line_no = maxy - header_line_no - FOOTER_LINE_NO
+        return pager_line_no
 
     def run(self):
         while not self.quit:
@@ -175,41 +204,47 @@ class IGrepper:
             char = self.win.getch()
             self.process_char(char)
 
-    def process_char(self, char):
-        if char in (CTRL_H, curses.KEY_BACKSPACE):  # backspace, ctrl-h
+    def process_char(self, char: int):
+        if char in (CTRL_H, curses.KEY_BACKSPACE):
             if len(self.regex) > 0:
                 self.regex = self.regex[:-1]
-        elif char == curses.KEY_RESIZE:
+        elif char in (CTRL_L, curses.KEY_RESIZE):
             pass  # The curses window has been resized, so re-render it
-        elif char == CTRL_N:  # ctrl-n
+        elif char == CTRL_N:
             if self.search.valid:
                 previous_match_objects = self.search.previous_searches + [self.search]
-                self.search = Search(self.search.output_lines())
+                self.search = Search([l.line_text for l in self.search.output_lines if not l.break_line])
                 self.search.previous_searches = previous_match_objects
+                self.search.context = previous_match_objects[-1].context
                 self.regex = ''
-        elif char == CTRL_P:  # ctrl-p
+        elif char == CTRL_P:
             if self.search.previous_searches:
                 self.search = self.search.previous_searches.pop()
                 self.regex = self.search.regex
         elif char == CTRL_O:
             self.toggle_display_mode()
-        elif char == CTRL_J:  # ctrl-j
+        elif char == CTRL_J:
             self.search.next_match()
-        elif char == CTRL_K:  # ctrl-k
+        elif char == CTRL_K:
             self.search.prev_match()
-        elif char == CTRL_G:  # ctrl-g
+        elif char == CTRL_G:
             self.win.erase()
             self.win.refresh()
             curses.endwin()
             grep_commands = []
             for m in self.search.previous_searches + [self.search]:
-                grep_cmd = "grep {}'{}'".format('-i ' if m.ignore_case else '', m.regex.replace("'", "\\'"))
+                options = ''
+                if m.ignore_case:
+                    options += '-i '
+                if m.context > 0:
+                    options += '-C {} '.format(m.context)
+                grep_cmd = "grep {}'{}'".format(options, m.regex.replace("'", "\\'"))
                 grep_commands.append(grep_cmd)
             to_yank = ' | '.join(grep_commands)
             copy_to_clipboard(to_yank)
-            print('Copied to clipboard: ' + BOLD + INVERTED + to_yank + RESET)
+            print('Copied to clipboard: \n\n' + BOLD + INVERTED + to_yank + RESET + '\n')
             self.quit = True
-        elif char == CTRL_Y:  # ctrl-y
+        elif char == CTRL_Y:
             if not self.search.unique_matches:
                 return
             self.win.erase()
@@ -218,33 +253,38 @@ class IGrepper:
             to_yank = self.search.unique_matches[self.search.selected_match]
             if to_yank:
                 copy_to_clipboard(to_yank)
-                print('Copied to clipboard: ' + BOLD + INVERTED + to_yank + RESET)
+                print('Copied to clipboard: \n\n' + BOLD + INVERTED + to_yank + RESET + '\n')
                 self.quit = True
-        elif char == CTRL_I:  # ctrl-i
+        elif char == CTRL_V:
             self.search.ignore_case = not self.search.ignore_case
         elif char == curses.KEY_DOWN:
             self.pager_ypos += 1
         elif char == curses.KEY_NPAGE:
-            self.pager_ypos = self.pager_ypos + curses.LINES  # TODO scroll correct amount
+            self.pager_ypos = self.pager_ypos + self.get_number_of_pager_lines()
+        elif char == CTRL_D:
+            self.pager_ypos = self.pager_ypos + int(self.get_number_of_pager_lines() / 2)
         elif char == curses.KEY_UP:
             self.pager_ypos = max(0, self.pager_ypos - 1)
         elif char == curses.KEY_PPAGE:
-            self.pager_ypos = max(0, self.pager_ypos - curses.LINES)  # TODO scroll correct amount
+            self.pager_ypos = max(0, self.pager_ypos - self.get_number_of_pager_lines())
+        elif char == CTRL_U:
+            self.pager_ypos = max(0, self.pager_ypos - int(self.get_number_of_pager_lines() / 2))
         elif char == curses.KEY_RIGHT:
             self.pager_xpos += 1
         elif char == curses.KEY_LEFT:
             self.pager_xpos = max(0, self.pager_xpos - 1)
+        elif char == CTRL_R:
+            self.search.context = max(0, self.search.context - 1)
+        elif char == CTRL_T:
+            self.search.context += 1
         else:
             self.regex += chr(char)
 
-        maxy, _ = self.win.getmaxyx()
-        header_line_no = len(self.search.previous_searches) + 2
-        pager_line_no = maxy - header_line_no - FOOTER_LINE_NO
-        max_pager_ypos = max(0, len(self.search.matched_lines) - pager_line_no)
+        max_pager_ypos = max(0, len(self.search.output_lines) - self.get_number_of_pager_lines())
         self.pager_ypos = min(self.pager_ypos, max_pager_ypos)
 
 
-def render(search, window, pager_ypos, pager_xpos):
+def render(search: Search, window, pager_ypos: int, pager_xpos: int):
     more_lines_after = True
     maxy, maxx = window.getmaxyx()
     footer_div_ypos = maxy - 2
@@ -267,33 +307,52 @@ def render(search, window, pager_ypos, pager_xpos):
     window.addstr(header_char * maxx, header_color)
 
     # trim output to visible by pager
-    matched_lines = search.matched_lines
-    line_highlights = search.line_highlights
-    max_pager_ypos = max(0, len(matched_lines) - pager_line_no)
+    max_pager_ypos = max(0, len(search.output_lines) - pager_line_no)
     if pager_ypos >= max_pager_ypos:
         more_lines_after = False
-    line_highlights = line_highlights[pager_ypos:pager_ypos + pager_line_no]
-    lines_to_print_tmp = matched_lines[pager_ypos:pager_ypos + pager_line_no]
-    lines_to_print = [line[pager_xpos:pager_xpos + maxx - 1] for line in lines_to_print_tmp]
+    output_lines = search.output_lines[pager_ypos:pager_ypos + pager_line_no]
+
+    # Fill break lines with text to display
+    for l in output_lines:
+        if l.break_line:
+            l.line_text = '-' * maxx
+
+    line_text_to_print = [line.line_text[pager_xpos:pager_xpos + maxx - 1] for line in output_lines]
 
     # write output
-    window.addstr('\n'.join(lines_to_print))
+    window.addstr('\n'.join(line_text_to_print))
 
-    # highlight output
-    for lineno, line_match in enumerate(line_highlights):
-        for match in line_match:
-            if match.start < pager_xpos + maxx and match.end > pager_xpos:
+    # highlight break lines
+    for lineno, line_object in enumerate(output_lines):
+        if line_object.break_line:
+            color = curses.color_pair(32)
+            window.chgat(lineno + header_line_no, 0 - pager_xpos, maxx, color | curses.A_BOLD)
+
+    # highlight matches
+    for lineno, line_object in enumerate(output_lines):
+        for match in line_object.line_matches:
+            # Only highlight if inside pager view
+            def inside_pager(match_start, match_end, first_visible_x, last_visible_x):
+                if match_start < first_visible_x or match_start > last_visible_x:
+                    return False
+                if match_end < first_visible_x or match_end > last_visible_x:
+                    return False
+                return True
+
+            if inside_pager(match.start, match.end, pager_xpos, pager_xpos + maxx - 1):
                 if match.unique_id == search.selected_match:
                     color = curses.color_pair(31)
                 else:
                     color = curses.color_pair((match.unique_id % AVAILABLE_COLORS) + 1)
-                window.chgat(lineno + header_line_no, match.start, match.end - match.start,
+                window.chgat(lineno + header_line_no, match.start - pager_xpos, match.end - match.start,
                              color | curses.A_BOLD)
+
     # Footer
     footer_char, footer_color = ('v', curses.color_pair(31)) if more_lines_after else ('-', curses.color_pair(0))
     window.addstr(footer_div_ypos, 0, footer_char * maxx, footer_color)
     case_sensitivity_text = "[case insensitive], " if search.ignore_case else "[case sensitive],   "
     status_line = case_sensitivity_text + \
+                  'context: {}, '.format(search.context) + \
                   'lines: {}, '.format(search.number_of_matched_lines) + \
                   'matches: {}, '.format(search.match_count) + \
                   'unique: {},              '.format(search.unique_match_count) + \
@@ -309,7 +368,8 @@ def main():
 
     group.add_argument("-e", "--regexp", action="store", default='', help="Regular expression to preload")
     group.add_argument("-w", "--word", action="store_true", help="Preload the regular expression '\\S+'")
-    # parser.add_argument("-c", "--context", action="store", type=int, help="Print CONTEXT num of output context")
+    parser.add_argument("-c", "--context", action="store", type=int, default=0,
+                        help="Print CONTEXT num of output context")
 
     args = parser.parse_args()
 
@@ -332,7 +392,7 @@ def main():
     if args.word:
         initial_regex = WORD_REGEX
     try:
-        f = IGrepper(input_lines, initial_regex)
+        f = IGrepper(input_lines, initial_regex, initial_context=args.context)
         f.run()
     finally:
         curses.endwin()
